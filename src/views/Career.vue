24a9617ebb1c982305cd1951ce05a6e53e04a67f
welcome <anandhi@systimanx.com>
2019-02-20 23:26:00 +0530

    carrerp page updated

--- src/views/Career.vue
+++ src/views/Career.vue
@@ -1,26 +1,142 @@
 <template>
- <section class="hero is-fullheight is-default is-bold">
- <layout-header></layout-header>
-  <div class="career">
-    <p>This is an career page</p>
-    <p>
-      card  hover-border-outer hover-border - pedning
-form desing in contact page - peding
-responsive in all pages - peding
-
-    </p>
-  </div>
-    <layout-footer></layout-footer>
- </section>
-</template>
-<script>
-import LayoutHeader from '@/views/layouts/Header.vue';
-import LayoutFooter  from '@/views/layouts/Footer.vue';
-export default {
-  components: {
-    LayoutHeader,
-    LayoutFooter
-  }
-}
-</script>
-
+  <section class="hero is-fullheight is-default is-bold">
+  <layout-header></layout-header>
+     <div class="contactus section">
+       <div class="hero-body">
+       <div class="container">
+         <div class="subpage_block_entry">
+             <h4 class="banner-title-outer is-size-1 has-text-center"> Carrer </h4>
+         </div>
+         </div>
+     </div>
+   </div>
+    <section class="section">
+      <h4 class="has-text-left is-size-4" style="margin-bottom:12px;">Opening in all Postion</h4>
+      <div class="columns">
+         <!-- card one start -->
+          <div class="column">
+      <div class="card">
+        <div class="card-content">
+          <div class="media">
+            <div class="media-content">
+              <p class="title is-4">Web Developer</p>
+              <p class="subtitle is-6">PHP, Larevel 5</p>
+            </div>
+          </div>
+      
+          <div class="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+            <a href="#">#css</a> <a href="#">#responsive</a>
+            <br>
+            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- card one end -->
+    <!-- card one start -->
+    <div class="column">
+        <div class="card">
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4">Web Developer</p>
+                <p class="subtitle is-6">PHP, Larevel 5</p>
+              </div>
+            </div>
+        
+            <div class="content">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+              <a href="#">#css</a> <a href="#">#responsive</a>
+              <br>
+              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- card one end -->
+      <!-- card one start -->
+      <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <div class="media">
+                <div class="media-content">
+                  <p class="title is-4">Web Developer</p>
+                  <p class="subtitle is-6">PHP, Larevel 5</p>
+                </div>
+              </div>
+          
+              <div class="content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                <a href="#">#css</a> <a href="#">#responsive</a>
+                <br>
+                <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- card one end -->
+        <!-- card one start -->
+        <div class="column">
+            <div class="card">
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-content">
+                    <p class="title is-4">Web Developer</p>
+                    <p class="subtitle is-6">PHP, Larevel 5</p>
+                  </div>
+                </div>
+            
+                <div class="content">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                  <a href="#">#css</a> <a href="#">#responsive</a>
+                  <br>
+                  <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- card one end -->
+          <!-- card one start -->
+          <div class="column">
+              <div class="card">
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-content">
+                      <p class="title is-4">Web Developer</p>
+                      <p class="subtitle is-6">PHP, Larevel 5</p>
+                    </div>
+                  </div>
+              
+                  <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                    <a href="#">#css</a> <a href="#">#responsive</a>
+                    <br>
+                    <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- card one end -->
+    </div>
+    </section>
+     <layout-footer></layout-footer>
+  </section>
+ </template>
+ <script>
+ import LayoutHeader from '@/views/layouts/Header.vue';
+ import LayoutFooter  from '@/views/layouts/Footer.vue';
+ export default {
+   components: {
+     LayoutHeader,
+     LayoutFooter
+   }
+ }
+ </script>
+ 
+ 
